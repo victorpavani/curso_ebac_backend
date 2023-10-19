cbf9938a8435784cba919b73bc0014c8cfa29e96
Victor Pavani <77660524+victorpavani@users.noreply.github.com>
2023-10-19 19:07:46 -0300

    Tarefa Calculadora Media
    Tarefa Modulo 8 com operadores

---
+++ TarefaCalculoMedio/Calculadora/src/CalculadoraMedia.java
@@ -0,0 +1,15 @@
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        // Defina as notas diretamente no código
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 6.5;
+        double nota4 = 9.0;
+
+        // Calcule a média das notas
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        // Imprima o resultado no console
+        System.out.println("A média das notas é: " + media);
+    }
+}
