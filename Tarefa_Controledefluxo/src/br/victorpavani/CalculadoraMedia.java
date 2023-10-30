112eaa697e9fb8fffa4a290e8561756cf5cef843
Victor Pavani <77660524+victorpavani@users.noreply.github.com>
2023-10-30 17:49:20 -0300

    Tarefa Controle de fluxo
    Tarefa controle de fluxo, onde foi criado uma calculadora que consome 4 notas e faz a sua media, e retorna uma mensagem de aprovacao ou reprovacao.

---
+++ Tarefa_Controledefluxo/src/br/victorpavani/CalculadoraMedia.java
@@ -0,0 +1,33 @@
+package br.victorpavani;
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota: ");
+        double nota4 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (media < 7) {
+            System.out.println("Média: " + media);
+            System.out.println("Reprovado");
+        } else {
+            System.out.println("Média: " + media);
+            System.out.println("Aprovado");
+        }
+
+        scanner.close();
+    }
+}
